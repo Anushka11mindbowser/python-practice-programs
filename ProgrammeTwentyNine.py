2045e94ada95b3bce700883fc3247694e721e2e4
Anushka11mindbowser <100129927+Anushka11mindbowser@users.noreply.github.com>
2022-04-07 14:03:12 +0530

    ProgrammeTwentyNineDone!

--- ProgrammeTwentyNine.py
+++ ProgrammeTwentyNine.py
@@ -1,17 +1,16 @@
-lst = []
-s = int(input("Enter the starting range: \n"))
-e = int(input("Enter the ending range: \n"))
-for i in range(s, e+1):
-    a = str(i)
-    for j in range(0,len(str(i))):
-        if j % 2 == 0:
-         lst.append(j)
-print(lst)
+start = int(input("Enter the starting the range: \n"))
+end = int(input("Enter the ending the range: \n"))
+res_lst = []
+def even_digits():
+    for i in range(start,end+1):
+        a = str(i)
+        b = list(a)
+        for j in b:
+            if int(j) % 2 == 0:
+                res_lst.append(i)
+    print(res_lst)
 
-
-
-
-
+print(even_digits())
 
 
 
