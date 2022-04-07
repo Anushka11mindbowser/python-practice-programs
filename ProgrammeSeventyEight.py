7960b43cece36907a0501017c6bb69df0da88a21
Anushka11mindbowser <100129927+Anushka11mindbowser@users.noreply.github.com>
2022-04-07 13:30:59 +0530

    ProgrammeSeventyEightUpdated!

--- ProgrammeSeventyEight.py
+++ ProgrammeSeventyEight.py
@@ -6,10 +6,10 @@ input_string = input("Enter a string: \n")
 
 
 
-
+#Approach One: Converting the list into set to find the difference
 def missing_alphabets(strng):
     if strng.islower():
-
+        #applying difference methods on sets to find the missing alphabets
         res_set = set(alphabets_list).difference(set(strng))
         if len(res_set) == 0:
             return "No Missing Alphabets"
@@ -22,10 +22,14 @@ def missing_alphabets(strng):
 
 print(missing_alphabets(input_string))
 
+#Approach Two: Using for loop and traversing both the lists
 def alphabets_missing(strng):
+    #Initializing an empty list to append the missing alphabets
     res_lst = []
     for i in alphabets_list:
         if i not in strng:
             res_lst.append(i)
-    return sorted(res_lst)
+        return sorted(res_lst)
+    else:
+        return "No Missing Alphabets"
 print(alphabets_missing(input_string))
