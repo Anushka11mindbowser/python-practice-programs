781f68d5fd3d13d9078e2fe54fbb40cde2a5606f
Anushka11mindbowser <100129927+Anushka11mindbowser@users.noreply.github.com>
2022-04-07 13:26:51 +0530

    ProgrammeSeventyEightCompleted!

--- ProgrammeSeventyEight.py
+++ ProgrammeSeventyEight.py
@@ -1,2 +1,31 @@
-def missing_alphabets():
-    input_string = input("Enter a string\n")
+import string
+alphabets_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+input_string = input("Enter a string: \n")
+
+
+
+
+def missing_alphabets(strng):
+    if strng.islower():
+
+        res_set = set(alphabets_list).difference(set(strng))
+        if len(res_set) == 0:
+            return "No Missing Alphabets"
+        else:
+            res_lst = list(res_set)
+            return sorted(res_lst)
+    else:
+        return "Enter a string in lower case"
+
+
+print(missing_alphabets(input_string))
+
+def alphabets_missing(strng):
+    res_lst = []
+    for i in alphabets_list:
+        if i not in strng:
+            res_lst.append(i)
+    return sorted(res_lst)
+print(alphabets_missing(input_string))
